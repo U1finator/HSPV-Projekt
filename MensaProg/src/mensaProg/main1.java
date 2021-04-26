5c462e045e144447648ed60fb40f64cea06df3d5
Julia <Julia@DESKTOP-J7CIJ5R.fritz.box>
2021-04-26 14:50:17 +0200

    added YUI

---
+++ MensaProg/src/mensaProg/main1.java
@@ -0,0 +1,18 @@
+package mensaProg;
+
+public class main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		IDandPW idandpw = new IDandPW();
+		
+		
+		LoginPage logininfo = new LoginPage(idandpw.getLoginInfo());
+		
+		
+		
+
+	}
+
+}
