b83719d169d2ebe0d4b95cc7c6d8b4b41e5efbbe
Admin <Admin@VINF-MS2048-W10>
2021-04-26 16:28:04 +0200

    icons hinzugefügt

--- MensaProg/src/mensaProg/MainGUI.java
+++ MensaProg/src/mensaProg/MainGUI.java
@@ -41,6 +41,8 @@ import java.awt.Font;
 import javax.swing.JMenu;
 import javax.swing.BoxLayout;
 import java.awt.GridLayout;
+import javax.swing.ImageIcon;
+import javax.swing.border.CompoundBorder;
 
 public class MainGUI {
 
@@ -76,6 +78,8 @@ public class MainGUI {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(Color.WHITE);
+		frame.setBackground(Color.WHITE);
 		frame.setBounds(100, 100, 652, 555);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
@@ -157,6 +161,8 @@ public class MainGUI {
 		frame.getContentPane().add(lblNewLabel_23, gbc_lblNewLabel_23);
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_1.setBackground(new Color(153, 204, 204));
 		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
 		gbc_panel_1.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_1.fill = GridBagConstraints.BOTH;
@@ -178,6 +184,7 @@ public class MainGUI {
 		panel_1.add(lblNewLabel_31, BorderLayout.NORTH);
 		
 		JPanel panel_4 = new JPanel();
+		panel_4.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
 		gbc_panel_4.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_4.fill = GridBagConstraints.BOTH;
@@ -190,16 +197,19 @@ public class MainGUI {
 		panel_4.add(lblNewLabel_9);
 		
 		JLabel lblNewLabel_24 = new JLabel("Pommes");
+		lblNewLabel_24.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_4.add(lblNewLabel_24);
-		
-		JLabel lblNewLabel_25 = new JLabel("5\u20AC");
-		panel_4.add(lblNewLabel_25);
 		
 		JLabel lblNewLabel_28 = new JLabel("Gericht 1");
 		lblNewLabel_28.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_4.add(lblNewLabel_28, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_25 = new JLabel("3\u20AC");
+		lblNewLabel_25.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_4.add(lblNewLabel_25, BorderLayout.SOUTH);
+		
 		JPanel panel_13 = new JPanel();
+		panel_13.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_13 = new GridBagConstraints();
 		gbc_panel_13.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_13.fill = GridBagConstraints.BOTH;
@@ -212,7 +222,16 @@ public class MainGUI {
 		lblNewLabel_12.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_13.add(lblNewLabel_12, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_26 = new JLabel("Nudeln");
+		lblNewLabel_26.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_13.add(lblNewLabel_26, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_42 = new JLabel("New label");
+		lblNewLabel_42.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_13.add(lblNewLabel_42, BorderLayout.SOUTH);
+		
 		JPanel panel_14 = new JPanel();
+		panel_14.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_14 = new GridBagConstraints();
 		gbc_panel_14.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_14.fill = GridBagConstraints.BOTH;
@@ -225,8 +244,17 @@ public class MainGUI {
 		lblNewLabel_15.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_14.add(lblNewLabel_15, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_43 = new JLabel("New label");
+		lblNewLabel_43.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_14.add(lblNewLabel_43, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_44 = new JLabel("New label");
+		lblNewLabel_44.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_14.add(lblNewLabel_44, BorderLayout.SOUTH);
+		
 		JPanel panel = new JPanel();
-		panel.setBackground(Color.YELLOW);
+		panel.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel.setBackground(Color.WHITE);
 		GridBagConstraints gbc_panel = new GridBagConstraints();
 		gbc_panel.insets = new Insets(0, 0, 5, 0);
 		gbc_panel.fill = GridBagConstraints.BOTH;
@@ -249,6 +277,7 @@ public class MainGUI {
 		panel.add(lblNewLabel_30, BorderLayout.SOUTH);
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
 		gbc_panel_2.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_2.fill = GridBagConstraints.BOTH;
@@ -261,7 +290,16 @@ public class MainGUI {
 		lblNewLabel_7.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_2.add(lblNewLabel_7, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_32 = new JLabel("New label");
+		lblNewLabel_32.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_2.add(lblNewLabel_32, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_33 = new JLabel("New label");
+		lblNewLabel_33.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_2.add(lblNewLabel_33, BorderLayout.SOUTH);
+		
 		JPanel panel_5 = new JPanel();
+		panel_5.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_5 = new GridBagConstraints();
 		gbc_panel_5.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_5.fill = GridBagConstraints.BOTH;
@@ -274,7 +312,16 @@ public class MainGUI {
 		lblNewLabel_10.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_5.add(lblNewLabel_10, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_34 = new JLabel("New label");
+		lblNewLabel_34.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_5.add(lblNewLabel_34, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_35 = new JLabel("New label");
+		lblNewLabel_35.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_5.add(lblNewLabel_35, BorderLayout.SOUTH);
+		
 		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_7 = new GridBagConstraints();
 		gbc_panel_7.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_7.fill = GridBagConstraints.BOTH;
@@ -287,7 +334,16 @@ public class MainGUI {
 		lblNewLabel_13.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_7.add(lblNewLabel_13, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_36 = new JLabel("New label");
+		lblNewLabel_36.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_7.add(lblNewLabel_36, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_37 = new JLabel("New label");
+		lblNewLabel_37.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_7.add(lblNewLabel_37, BorderLayout.SOUTH);
+		
 		JPanel panel_9 = new JPanel();
+		panel_9.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_9 = new GridBagConstraints();
 		gbc_panel_9.insets = new Insets(0, 0, 5, 5);
 		gbc_panel_9.fill = GridBagConstraints.BOTH;
@@ -300,7 +356,16 @@ public class MainGUI {
 		lblNewLabel_16.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_9.add(lblNewLabel_16, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_38 = new JLabel("New label");
+		lblNewLabel_38.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_9.add(lblNewLabel_38, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_39 = new JLabel("New label");
+		lblNewLabel_39.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_9.add(lblNewLabel_39, BorderLayout.SOUTH);
+		
 		JPanel panel_11 = new JPanel();
+		panel_11.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panel_11 = new GridBagConstraints();
 		gbc_panel_11.insets = new Insets(0, 0, 5, 0);
 		gbc_panel_11.fill = GridBagConstraints.BOTH;
@@ -313,8 +378,17 @@ public class MainGUI {
 		lblNewLabel_18.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_11.add(lblNewLabel_18, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_40 = new JLabel("New label");
+		lblNewLabel_40.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_11.add(lblNewLabel_40, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_41 = new JLabel("New label");
+		lblNewLabel_41.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_11.add(lblNewLabel_41, BorderLayout.SOUTH);
+		
 		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(0, 128, 0));
+		panel_3.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		panel_3.setBackground(new Color(0, 255, 153));
 		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
 		gbc_panel_3.insets = new Insets(0, 0, 0, 5);
 		gbc_panel_3.fill = GridBagConstraints.BOTH;
@@ -327,7 +401,16 @@ public class MainGUI {
 		lblNewLabel_6.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_3.add(lblNewLabel_6, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_45 = new JLabel("New label");
+		lblNewLabel_45.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_3.add(lblNewLabel_45, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_48 = new JLabel("New label");
+		lblNewLabel_48.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_3.add(lblNewLabel_48, BorderLayout.SOUTH);
+		
 		JPanel panel_6 = new JPanel();
+		panel_6.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel_6.setBackground(Color.GREEN);
 		GridBagConstraints gbc_panel_6 = new GridBagConstraints();
 		gbc_panel_6.insets = new Insets(0, 0, 0, 5);
@@ -341,7 +424,16 @@ public class MainGUI {
 		lblNewLabel_11.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_6.add(lblNewLabel_11, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_46 = new JLabel("New label");
+		lblNewLabel_46.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_6.add(lblNewLabel_46, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_49 = new JLabel("New label");
+		lblNewLabel_49.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_6.add(lblNewLabel_49, BorderLayout.SOUTH);
+		
 		JPanel panel_8 = new JPanel();
+		panel_8.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel_8.setBackground(Color.GREEN);
 		GridBagConstraints gbc_panel_8 = new GridBagConstraints();
 		gbc_panel_8.insets = new Insets(0, 0, 0, 5);
@@ -355,7 +447,16 @@ public class MainGUI {
 		lblNewLabel_14.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_8.add(lblNewLabel_14, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_47 = new JLabel("New label");
+		lblNewLabel_47.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_8.add(lblNewLabel_47, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_50 = new JLabel("New label");
+		lblNewLabel_50.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_8.add(lblNewLabel_50, BorderLayout.SOUTH);
+		
 		JPanel panel_10 = new JPanel();
+		panel_10.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel_10.setBackground(Color.GREEN);
 		GridBagConstraints gbc_panel_10 = new GridBagConstraints();
 		gbc_panel_10.insets = new Insets(0, 0, 0, 5);
@@ -369,7 +470,16 @@ public class MainGUI {
 		lblNewLabel_17.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_10.add(lblNewLabel_17, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_51 = new JLabel("New label");
+		lblNewLabel_51.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_10.add(lblNewLabel_51, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_52 = new JLabel("New label");
+		lblNewLabel_52.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_10.add(lblNewLabel_52, BorderLayout.SOUTH);
+		
 		JPanel panel_12 = new JPanel();
+		panel_12.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		panel_12.setBackground(Color.GREEN);
 		GridBagConstraints gbc_panel_12 = new GridBagConstraints();
 		gbc_panel_12.fill = GridBagConstraints.BOTH;
@@ -382,19 +492,23 @@ public class MainGUI {
 		lblNewLabel_19.setHorizontalAlignment(SwingConstants.CENTER);
 		panel_12.add(lblNewLabel_19, BorderLayout.NORTH);
 		
+		JLabel lblNewLabel_53 = new JLabel("New label");
+		lblNewLabel_53.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_12.add(lblNewLabel_53, BorderLayout.CENTER);
+		
+		JLabel lblNewLabel_54 = new JLabel("New label");
+		lblNewLabel_54.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_12.add(lblNewLabel_54, BorderLayout.SOUTH);
+		
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
 		
 		JMenu mnNewMenu = new JMenu("Men\u00FC");
+		mnNewMenu.setIcon(new ImageIcon(MainGUI.class.getResource("/mensaProg/outline_menu_black_24dp.png")));
 		menuBar.add(mnNewMenu);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("New radio button");
-		mnNewMenu.add(rdbtnNewRadioButton);
-		
-		JSeparator separator = new JSeparator();
-		mnNewMenu.add(separator);
-		
 		JMenuItem mntmNewMenuItem = new JMenuItem("Einstellungen");
+		mntmNewMenuItem.setIcon(new ImageIcon(MainGUI.class.getResource("/mensaProg/outline_settings_black_24dp.png")));
 		mntmNewMenuItem.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
