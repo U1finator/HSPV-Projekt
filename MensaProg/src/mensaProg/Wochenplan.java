af9756f9761513085d1fabd05146e3c173710c13
U1finator <59577069+U1finator@users.noreply.github.com>
2021-04-30 09:14:27 +0200

    eine neue Methode um die Gerichte aus der Datenbank auszulesen und in eine Hashmap zu überführen

--- MensaProg/src/mensaProg/Wochenplan.java
+++ MensaProg/src/mensaProg/Wochenplan.java
@@ -6,6 +6,36 @@ import java.sql.Statement;
 
 public class Wochenplan {
 	
+	HashMap<String, String> gerichte = new HashMap<String, String>();
+	Connection conn = SqlConnector.dbConnector();	//eine Verbindung zur SQLite Datenbank wird hergestellt
+	
+	Wochenplan(){
+		try 
+		{
+			Statement stmt = conn.createStatement();
+			for(int i = 1; i <= stmt.executeQuery("SELECT MAX(id) FROM gerichte").toString().charAt(0); i++)	//mithilfe des Statements wird die Anzahl von Gerichten in der Datenbank abgefragt
+			{
+				gerichte.put(stmt.executeQuery("SELECT name FROM gerichte WHERE id="+i).getString(1), stmt.executeQuery("SELECT preis FROM gerichte WHERE id="+i).getString(1));	//das Gericht mit dazugehörigem Preis wird abgefragt
+			}
+		} 
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		try 
+		{
+			conn.close();	//die Verbindung zur Datenbank wird wieder geschlossen
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	//wenn jemand die Methode aufruft, bekommt er Login info
+	protected HashMap<String, String> getGerichte() {
+		return gerichte;
+	}
+	
 	public static void create()  {
 		Connection conn = SqlConnector.dbConnector();
 		Statement stmt;
