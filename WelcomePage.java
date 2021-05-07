d42071fac2956ae6f87c931ceacacdd60bb6d0a5
Shreki182 <81674541+Shreki182@users.noreply.github.com>
2021-05-07 15:47:00 +0200

    Benutzerdaten & Einstellungen
    windows

---
+++ WelcomePage.java
@@ -0,0 +1,37 @@
+package mensaProg;
+
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class WelcomePage {
+	
+	JFrame frame = new JFrame();
+	JLabel welcomeLabel = new JLabel("Einstellungen");
+	JLabel bankBalanceLabel = new JLabel("Kontostand");
+	JLabel nutritionLabel = new JLabel("Ernährung");
+	
+	WelcomePage(String userID) {
+		
+		welcomeLabel.setBounds(0,0,200,35);
+		welcomeLabel.setFont(new Font(null,Font.PLAIN,20));
+		welcomeLabel.setText("Hallo "+userID);
+		
+		bankBalanceLabel.setBounds(50,100,200,35);
+		bankBalanceLabel.setFont(new Font(null,Font.PLAIN,16));
+		bankBalanceLabel.setText("Kontostand: ");  //Einbindung Kontostand
+		
+		nutritionLabel.setBounds(50,120,200,35);
+		nutritionLabel.setFont(new Font(null,Font.PLAIN,16));
+		nutritionLabel.setText("Ernährung: ");
+		
+		frame.getContentPane().add(welcomeLabel);
+		frame.getContentPane().add(bankBalanceLabel);
+		frame.getContentPane().add(nutritionLabel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(420,420);
+		frame.getContentPane().setLayout(null);
+		frame.setVisible(true);
+	}
+}
